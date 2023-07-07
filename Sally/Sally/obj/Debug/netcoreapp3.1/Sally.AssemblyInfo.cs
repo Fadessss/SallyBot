db9d603078b20f880a430d8647b046b38bfa80f5
Jack-Briggs <66038932+Jack-Briggs@users.noreply.github.com>
2023-07-08 11:46:55 +1200

    Two new diaglogs added
    Payslips dialog
    
    Request leave dialog

--- Sally/Sally/obj/Debug/netcoreapp3.1/Sally.AssemblyInfo.cs
+++ Sally/Sally/obj/Debug/netcoreapp3.1/Sally.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sally")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed14b073519a7cce8151a09cc5cfbf188fc8228d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sally")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sally")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
