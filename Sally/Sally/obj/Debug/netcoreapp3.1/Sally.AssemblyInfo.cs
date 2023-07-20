359dd28635b1ffa89398b88bea61e2af428f1096
CDGibson97 <105181677+CDGibson97@users.noreply.github.com>
2023-07-20 19:52:24 +1200

    Adjusted Queries to be Adaptive Cards
    Currently not linked up anywhere, waiting to decide how we handle that.
    
    Also want to make a return dialog, but will wait before implementing into the software.

--- Sally/Sally/obj/Debug/netcoreapp3.1/Sally.AssemblyInfo.cs
+++ Sally/Sally/obj/Debug/netcoreapp3.1/Sally.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sally")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c568c3bd6c7b05aec823732ee46e98a70503659f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sally")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sally")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
